6fabbe6e9bdb3848a3013b761a172b1d0f53a3b0
numan947 <mahmudulhasan947@gmail.com>
2018-10-05 15:29:09 +0600

    I am a stupid and waste 3 hours solving a problem, that didn't even exist, UVA222

--- test.cpp
+++ test.cpp
@@ -1,40 +1,42 @@
-#include<cmath>
+#include<algorithm>
 #include<cstdio>
-#include<cstring>
  
 using namespace std;
  
-int T, N, tot;
-int peg[100];
+// distance, gallon capacity, mpg, cost of first filling
+double d, g, mpg, c;
+// station distance, station cost
+double sd[100], sc[100];
+int n; // number of gas stations
  
-void simul(int v, int p) {
-    if(p == N) return;
-    if(peg[p] == 0) {
-        peg[p] = v;
-        tot++;
-        return simul(v + 1, p);
-    }
-     
-    for(int i = 0; i <= p; i++) {
-        int root = (int) sqrt(peg[i] + v);
-        if(root * root == peg[i] + v) {
-            peg[i] = v;
-            tot++;
-            return simul(v + 1, p);
+double simul(double distance, int station) {
+    if(distance + g * mpg >= d || station == n)
+        return 0;
+    
+    printf("%lf\n",g);
+    double best = 1000000;
+    for(int i = station; i < n; i++) {
+        double fuel_used = (sd[i] - distance) / mpg;
+        if(g - fuel_used < 0) break;
+        if(g - fuel_used <= g / 2.0) {
+            best = min(best, 200 + fuel_used * sc[i] + simul(sd[i], i + 1));
+        } else if(g - fuel_used > g / 2.0 && i + 1 < n && sd[i + 1] > distance + g * mpg) {
+            return 200 + fuel_used * sc[i] + simul(sd[i], i + 1);
         }
     }
  
-    simul(v, p + 1);
+    return best;
 }
  
 int main() {
     freopen("input.txt", "r", stdin);
-    scanf("%d", &T);
-    while(T--) {
-        scanf("%d", &N);
-        memset(peg, 0, sizeof peg);
-        tot = 0;
-        simul(1, 0);
-        printf("%d\n", tot);
+    for(int T = 1;; T++) {
+        scanf("%lf", &d);
+        if(d == -1) break;
+        scanf("%lf %lf %lf %d", &g, &mpg, &c, &n);
+        for(int i = 0; i < n; i++)
+            scanf("%lf %lf", &sd[i], &sc[i]);
+        printf("Data Set #%d\n", T);
+        printf("minimum cost = $%.2f\n", c + simul(0, 0)/100);
     }
 }
