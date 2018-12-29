a9a99f850b743beecb5a9554c087172ac9f10607
numan947 <mahmudulhasan947@gmail.com>
2018-12-29 19:12:38 +0600

    Solved some more problems

--- asdf.cpp
+++ asdf.cpp
@@ -1,39 +1,83 @@
-#include <iostream>
-#include <fstream>
+/*
+    AUST_royal.flush
+*/
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ms(a,v) memset(a,v,sizeof a)
+#define II ({int a; scanf("%d", &a); a;})
+#define LL ({Long a; scanf("%lld", &a); a;})
+#define DD ({double a; scanf("%lf", &a); a;})
+#define EPS 1e-10
+#define pi 3.1415926535897932384626433832795
 using namespace std;
 
-int ord[21], arr[21], lis[21], n;
+typedef long long Long;
+typedef unsigned long long ull;
+typedef vector<int> vi ;
+typedef set<int> si;
+typedef vector<Long>vl;
+typedef pair<int,int>pii;
+typedef pair<Long,Long>pll;
+typedef pair<double,double>pdd;
 
-int main() {
-  freopen("input.txt", "r", stdin);
-  int in;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    cin >> in;
-    ord[i+1] = in;
-  }
-  for(int i=1;i<=n;i++)
-    cout<<ord[i]<<" ";
-  cout<<endl;
-  while (cin >> in) { 
-    arr[in] = 1;
-    lis[1] = 1;
-    for (int i = 2; i <= n; i++) {
-      cin >> in;
-      arr[in] = i, lis[i] = 1;
-    }
-  for(int i=1;i<=n;i++)
-    cout<<arr[i]<<" ";
-  cout<<endl;
+#define forab(i, a, b)	for (__typeof (b) i = (a) ; i <= b ; ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof (b)i = (b) ; i >= a ; --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
 
-    int m = 1;
-    for (int i = 2; i <= n; i++)
-      for (int j = 1; j < i; j++)
-        if (ord[arr[i]] > ord[arr[j]] and lis[i] < lis[j] + 1) {
-          lis[i] = lis[j] + 1;
-          m = max(m, lis[i]);
+const int MX = 307 ;
+const int INF = 1e8 + 7;
+
+
+#define MX 200007
+const int inf = 1e8 + 7 ;
+struct Node{ int x,y ; };
+
+Node Block[MX];
+int N ;
+
+// condition : x2 > x1 and y2 > y1
+bool cmp(Node a,Node b){
+    if( a.y == b.y ) return a.x < b.x;
+    return a.y > b.y;
+}
+
+int Doll(){
+    vi v ;
+    sort(Block,Block+N,cmp);
+    for(int i=0;i<N;i++)
+        cout<<Block[i].x<<" "<<Block[i].y<<endl;
+    v.push_back(inf);
+    int ans = 0;
+    rep(i,N) {
+        int now = Block[i].x;
+        int Idx = lower_bound(all(v),now+1)-v.begin();
+        if( Idx == v.size()-1) {
+            ans++;
+            v.push_back(inf);
         }
-    cout << m << endl;
-  }
-  return 0;
+        v[Idx] = now;
+    }
+    for(int i=0;i<v.size();i++)
+        cout<<v[i]<<" ";
+    cout<<endl;
+    return ans;
+}
+
+
+int main(){
+    
+    freopen ("input.txt", "r", stdin);
+       //freopen ("out.txt","w",stdout);
+    
+    int test = II ;
+    For(cs,test){
+        N = II ;
+        rep(i,N) Block[i].x = II , Block[i].y = II ;
+        printf("%d\n",Doll());
+    }
 }
